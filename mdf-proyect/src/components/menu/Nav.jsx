dfd97e2e7da0cfa4225537eb99701ef4042cd686
narahumano1989 <humano.naraesther@gmail.com>
2023-08-09 14:55:22 -0300

    version final de portada

--- mdf-proyect/src/components/menu/Nav.jsx
+++ mdf-proyect/src/components/menu/Nav.jsx
@@ -3,7 +3,7 @@ function Nav() {
     return (
 
         <div className=" w-full fixed bg-[#f5f5f5]">
-            <ul className=" fixed flex flex-row gap-5 w-full mt-0 pl-[55%] content-center items-center right-0 top-15 bg-[#f5f5f5] p-5">
+            <ul className="absolute flex flex-row gap-5 w-full mt-0 pl-[55%] content-center items-center right-0 top-15 bg-[#f5f5f5] p-5">
                 <li ><a href="#" className="text-center flex flex-row justify-center items-center justify-items-center content-center gap-2">Productos <span class="material-symbols-outlined text-[#C4AB84] font-bold">expand_more</span></a></li>
                 <li><a href="#" className="text-center flex flex-row justify-center items-center justify-items-center content-center gap-2">Método Montessori <span class="material-symbols-outlined text-[#C4AB84] font-bold">expand_more</span></a></li>
                 <li><a href="#" className="text-center flex flex-row justify-center items-center justify-items-center content-center gap-2">¿Cómo Pintar? <span class="material-symbols-outlined text-[#C4AB84] font-bold">expand_more</span></a></li>
--- mdf-proyect/src/components/principal/home.jsx
+++ mdf-proyect/src/components/principal/home.jsx
@@ -1,13 +1,16 @@
 import logo from '../../assets/img/mdf-logo.png'
 import portada from '../../assets/img/niño-mdf.jpg'
+import Nav from '../menu/Nav.jsx'
 function Home() {
     return (
         <div>
+            <div className='h-14 vector-fondo'></div>
+            <Nav />
             <div id="logo">
-                <img src={logo} alt="logo mdf" className='ml-[10%] mt-[15px] w-[210.40px] h-[210.40px] rounded-full absolute z-1'/>
+                <img src={logo} alt="logo mdf" className='ml-[10%] mt-[15px] w-[200px] h-[205.40px] rounded-full absolute z-1'/>
             </div>
             <div id="portada">
-                <img src={portada} alt='foto portada mdf' className='w-[99%] h-[60%] m-auto'/>
+                <img src={portada} alt='foto portada mdf' className='w-full h-[900px] m-auto'/>
             </div>
         </div>
     )
